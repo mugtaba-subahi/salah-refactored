e934b243bb462ab15cfdfdd0818e5b04e96385b6
Mugtaba <m_subahi@hotmail.com>
2020-10-12 18:46:22 +0100

    removed mock data

--- src/helpers/setData.ts
+++ src/helpers/setData.ts
@@ -2,27 +2,10 @@ import { IApiPrayers, IPrayer } from '../interfaces';
 import Store from '../store';
 
 export default async (): Promise<void> => {
-  // const key: string = '2a99f189-6e3b-4015-8fb8-ff277642561d';
-  // const url: string = `https://www.londonprayertimes.com/api/times/?format=json&key=${key}`;
-  // const response = await fetch(url);
-  // const data: IApiPrayers = await response.json();
-
-  const data: IApiPrayers = {
-    fajr: '17:22',
-    fajr_jamat: '17:22',
-    sunrise: '17:23',
-    dhuhr: '17:24',
-    dhuhr_jamat: '17:22',
-    asr: '06:23',
-    asr_2: '17:22',
-    asr_jamat: '17:22',
-    date: '2020-17:22',
-    magrib: '17:26',
-    magrib_jamat: '17:22',
-    isha: '17:27',
-    isha_jamat: '00:01',
-    city: 'london'
-  };
+  const key: string = '2a99f189-6e3b-4015-8fb8-ff277642561d';
+  const url: string = `https://www.londonprayertimes.com/api/times/?format=json&key=${key}`;
+  const response = await fetch(url);
+  const data: IApiPrayers = await response.json();
 
   const prayers: IPrayer[] = Store.english.map(
     (name: string, index: number): IPrayer => {
