b528af6a2e16db98d7496b214b51be80acd36c2e
Mugtaba <m_subahi@hotmail.com>
2020-10-17 22:30:52 +0100

    added types

--- __tests__/helpers/stringToMilitary.spec.ts
+++ __tests__/helpers/stringToMilitary.spec.ts
@@ -6,12 +6,10 @@ import validateTimeHelper from '../../src/helpers/validateTime';
 import handler from '../../src/helpers/stringToMilitary';
 
 describe('stringToMilitary helper', () => {
-  const convertTimeMock = convertTime as jest.MockedFunction<typeof convertTime>;
-  const validateTimeHelperMock = validateTimeHelper as jest.MockedFunction<typeof validateTimeHelper>;
+  const convertTimeMock = convertTime as jest.Mock;
+  const validateTimeHelperMock = validateTimeHelper as jest.Mock;
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  afterEach(() => jest.clearAllMocks());
 
   it('should fail when passing invalid integer time type', done => {
     validateTimeHelperMock.mockReturnValueOnce(false);
@@ -55,19 +53,19 @@ describe('stringToMilitary helper', () => {
   it('should pass without converting time if Fajr, Sunrise or random input', done => {
     validateTimeHelperMock.mockReturnValueOnce(true);
 
-    const fajr = handler('Fajr', '15:15');
+    const fajr: string = handler('Fajr', '15:15');
     expect(fajr).toBe('15:15');
     expect(convertTimeMock).toBeCalledTimes(0);
 
     validateTimeHelperMock.mockReturnValueOnce(true);
 
-    const sunrise = handler('Sunrise', '18:18');
+    const sunrise: string = handler('Sunrise', '18:18');
     expect(sunrise).toBe('18:18');
     expect(convertTimeMock).toBeCalledTimes(0);
 
     validateTimeHelperMock.mockReturnValueOnce(true);
 
-    const anyInput = handler('any input', '18:18');
+    const anyInput: string = handler('any input', '18:18');
     expect(anyInput).toBe('18:18');
     expect(convertTimeMock).toBeCalledTimes(0);
 
@@ -77,7 +75,7 @@ describe('stringToMilitary helper', () => {
   it('should pass without converting time for Dhuhr if time is before 12pm', done => {
     validateTimeHelperMock.mockReturnValueOnce(true);
 
-    const time = handler('Dhuhr', '10:12');
+    const time: string = handler('Dhuhr', '10:12');
     expect(time).toBe('10:12');
     expect(convertTimeMock).toBeCalledTimes(0);
 
@@ -88,7 +86,7 @@ describe('stringToMilitary helper', () => {
     validateTimeHelperMock.mockReturnValueOnce(true);
     convertTimeMock.mockReturnValueOnce('12:00');
 
-    const time = handler('Dhuhr', '00:00');
+    const time: string = handler('Dhuhr', '00:00');
     expect(time).toBe('12:00');
     expect(convertTimeMock).toBeCalledTimes(1);
 
@@ -99,7 +97,7 @@ describe('stringToMilitary helper', () => {
     validateTimeHelperMock.mockReturnValueOnce(true);
     convertTimeMock.mockReturnValueOnce('13:35');
 
-    const time = handler('Dhuhr', '01:35');
+    const time: string = handler('Dhuhr', '01:35');
     expect(time).toBe('13:35');
     expect(convertTimeMock).toBeCalledTimes(1);
 
@@ -110,7 +108,7 @@ describe('stringToMilitary helper', () => {
     validateTimeHelperMock.mockReturnValueOnce(true);
     convertTimeMock.mockReturnValueOnce('14:53');
 
-    const time = handler('Asr', '02:53');
+    const time: string = handler('Asr', '02:53');
     expect(time).toBe('14:53');
     expect(convertTimeMock).toBeCalledTimes(1);
 
@@ -121,7 +119,7 @@ describe('stringToMilitary helper', () => {
     validateTimeHelperMock.mockReturnValueOnce(true);
     convertTimeMock.mockReturnValueOnce('18:33');
 
-    const time = handler('Magrib', '06:33');
+    const time: string = handler('Magrib', '06:33');
     expect(time).toBe('18:33');
     expect(convertTimeMock).toBeCalledTimes(1);
 
@@ -132,7 +130,7 @@ describe('stringToMilitary helper', () => {
     validateTimeHelperMock.mockReturnValueOnce(true);
     convertTimeMock.mockReturnValueOnce('21:27');
 
-    const time = handler('Isha', '09:27');
+    const time: string = handler('Isha', '09:27');
     expect(time).toBe('21:27');
     expect(convertTimeMock).toBeCalledTimes(1);
 
